f28728e082bb2907b1cf4ed762829fee78514798
Edgar Chen <edgarc@andrew.cmu.edu>
2015-05-19 16:33:51 -0700

    now with 100% more correct influxDB submissions

--- collect.js
+++ collect.js
@@ -20,28 +20,48 @@ var main = async function () {
 		connectionString: cfg.get('influxdb:connectionString')
 	});
 	function statMessage (message) {
-		debug(message.payload.status.taskId);
-		influx.addPoint('taskId',{
-			taskId:message.payload.status.taskId,
-			time:3
-		});
+		var runs=message.payload.status.runs;
+		var idx=runs.length-1
+		var run=runs[idx]
+		function statRun (run) {
+			var scheduled=(Date.parse(run.scheduled));
+			var started=(Date.parse(run.started));
+			var resolved=(Date.parse(run.resolved));
+			influx.addPoint('pendingDuration',{
+				provisionerId:message.payload.status.provisionerId,
+				workerType:message.payload.status.workerType,
+				time:scheduled,
+				duration:started-scheduled
+			});
+			influx.addPoint('runningDuration',{
+				provisionerId:message.payload.status.provisionerId,
+				workerType:message.payload.status.workerType,
+				time:started,
+				duration:resolved-started
+			});
+			influx.addPoint('reasonResolved',{
+				provisionerId:message.payload.status.provisionerId,
+				workerType:message.payload.status.workerType,
+				time:resolved,
+				count:1,
+				reasonResolved:run.reasonResolved
+			});
+			if(run.reasonCreated==='retry'&&idx>0)
+				statRun(runs[--idx]);
+		}
+		statRun(run)
 	}
-	var creds={credentials : {
+	var connection=new taskcluster.PulseConnection({
 	    	username: cfg.get('pulse:username'),      // Pulse username from pulse guardian
 	    	password: cfg.get('pulse:password')       // Pulse password from pulse guardian
-		}
-	}
-	var completedListener = new taskcluster.PulseListener(creds);
-	var failedListener = new taskcluster.PulseListener(creds);
-	var exceptionListener = new taskcluster.PulseListener(creds);	
+		});
+	var completedListener = new taskcluster.PulseListener({connection:connection});
+	var failedListener = new taskcluster.PulseListener({connection:connection});
+	var exceptionListener = new taskcluster.PulseListener({connection:connection});	
 	var queueEvents = new taskcluster.QueueEvents();
 	completedListener.bind(queueEvents.taskCompleted({})).catch(error => debug('Bind error: %s',error));
 	failedListener.bind(queueEvents.taskFailed({})).catch(error => debug('Bind error: %s',error));
 	exceptionListener.bind(queueEvents.taskException({})).catch(error => debug('Bind error: %s',error));
-	debug('i tried');
-	// completedListener.on('message',message => debug('completed'+JSON.stringify(message,null,2)));
-	// failedListener.on('message',message => debug('failed'+JSON.stringify(message,null,2)));
-	// exceptionListener.on('message',message => debug('exception'+JSON.stringify(message,null,2)));
 	completedListener.on('message',statMessage);
 	failedListener.on('message',statMessage);
 	exceptionListener.on('message',statMessage);
@@ -54,6 +74,7 @@ var main = async function () {
 		 completedListener.close();
 		 failedListener.close();
 		 exceptionListener.close();
+		 connection.close();
 		 influx.close()
 	},10000);
 }
