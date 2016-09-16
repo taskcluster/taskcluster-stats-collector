138bc53ba1cacd4e2be04a5c2ec439e2822895b3
Brian Yip <itsbriany@gmail.com>
2016-09-16 17:22:16 -0300

    Listener test only runs when clientId is set

--- test/listener_test.js
+++ test/listener_test.js
@@ -1,87 +1,97 @@
 suite('TaskListener', () => {
-  test('listens', async () => {
-    var debug = require('debug')('test:test');
-    var assert = require('assert');
-    var TaskListener = require('../lib/listener.js');
-    var slugid = require('slugid');
-    var taskcluster = require('taskcluster-client');
-    var base = require('taskcluster-base');
-    var monitoring = require('taskcluster-lib-monitor');
+  var debug = require('debug')('test:test');
+  var assert = require('assert');
+  var TaskListener = require('../lib/listener.js');
+  var slugid = require('slugid');
+  var taskcluster = require('taskcluster-client');
+  var base = require('taskcluster-base');
+  var monitoring = require('taskcluster-lib-monitor');
 
+  let listenerTest = async () => {
     let cfg = base.config({profile: 'test'});
+    if (cfg.taskcluster.credentials.clientId !== undefined) {
+      return test('listens', async() => {
+        var taskdefn = {
+          provisionerId: 'stats-provisioner',
+          workerType: 'stats-dummy',
+          payload: {},
+          created: taskcluster.fromNowJSON(),
+          deadline: taskcluster.fromNowJSON('2 hours'),
+          metadata: {
+            name: 'Testing!',
+            description: 'testing?',
+            owner: 'eggylv999@gmail.com',
+            source: 'https://github.com/taskcluster/taskcluster-stats-collector',
+          },
+        };
 
-    var taskdefn = {
-      provisionerId: 'stats-provisioner',
-      workerType: 'stats-dummy',
-      payload: {},
-      created: taskcluster.fromNowJSON(),
-      deadline: taskcluster.fromNowJSON('2 hours'),
-      metadata: {
-        name: 'Testing!',
-        description: 'testing?',
-        owner: 'eggylv999@gmail.com',
-        source: 'https://github.com/taskcluster/taskcluster-stats-collector',
-      },
-    };
+        console.log(JSON.stringify(cfg));
 
-    assert(cfg.pulse, 'pulse credentials required');
-    assert(cfg.taskcluster, 'taskcluster credentials required');
+        assert(cfg.pulse, 'pulse credentials required');
+        assert(cfg.taskcluster, 'taskcluster credentials required');
 
-    let monitor = await monitoring({
-      project: 'tc-stats-collector',
-      credentials: {clientId: 'fake', accessToken: 'alsofake'},
-      mock: true,
-    });
+        let monitor = await monitoring({
+          project: 'tc-stats-collector',
+          credentials: {clientId: 'fake', accessToken: 'alsofake'},
+          mock: true,
+        });
 
-    let listener = new TaskListener({
-      credentials: cfg.pulse,
-      queueName: undefined,
-      routingKey: {
-        provisionerId: 'stats-provisioner',
-      },
-      monitor,
-    });
+        let listener = new TaskListener({
+          credentials: cfg.pulse,
+          queueName: undefined,
+          routingKey: {
+            provisionerId: 'stats-provisioner',
+          },
+          monitor,
+        });
 
-    let task_messages = [];
-    listener.on('task-message', ({action}) => task_messages.push(action));
-    await listener.start();
+        let task_messages = [];
+        listener.on('task-message', ({action}) => task_messages.push(action));
+        await listener.start();
 
-    var id = slugid.v4();
-    var queue = new taskcluster.Queue({
-      credentials: cfg.taskcluster.credentials,
-    });
+        var id = slugid.v4();
+        var queue = new taskcluster.Queue({
+          credentials: cfg.taskcluster.credentials,
+        });
 
-    let result = await queue.createTask(id, taskdefn);
-    assert(result);
-    debug('task created');
+        let result = await queue.createTask(id, taskdefn);
+        assert(result);
+        debug('task created');
 
-    await queue.claimTask(id, 0, {
-      workerGroup:    'my-worker-group',
-      workerId:       'my-worker',
-    });
-    debug('task claimed');
+        await queue.claimTask(id, 0, {
+          workerGroup:    'my-worker-group',
+          workerId:       'my-worker',
+        });
+        debug('task claimed');
 
-    await queue.reportException(id, 0, {reason: 'worker-shutdown'});
-    debug('task exception');
+        await queue.reportException(id, 0, {reason: 'worker-shutdown'});
+        debug('task exception');
 
-    await queue.claimTask(id, 1, {
-      workerGroup:    'my-worker-group',
-      workerId:       'my-worker',
-    });
-    debug('task claimed again');
+        await queue.claimTask(id, 1, {
+          workerGroup:    'my-worker-group',
+          workerId:       'my-worker',
+        });
+        debug('task claimed again');
 
-    await queue.reportCompleted(id, 1);
-    debug('task completed');
+        await queue.reportCompleted(id, 1);
+        debug('task completed');
 
-    await listener.close();
+        await listener.close();
 
-    assert.deepEqual(task_messages, [
-      'task-pending',
-      'task-running',
-      'task-pending',
-      'task-running',
-      'task-completed',
-    ]);
-  });
+        assert.deepEqual(task_messages, [
+          'task-pending',
+          'task-running',
+          'task-pending',
+          'task-running',
+          'task-completed',
+        ]);
+      });
+    }
+
+    console.warn('Skipping Listening Test: Need clientId in configuration');
+    return test.skip('listens', async() => { });
+  };
+
+  listenerTest();
 });
 
